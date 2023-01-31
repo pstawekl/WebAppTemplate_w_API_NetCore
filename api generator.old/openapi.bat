44554087f0b914ea592622c0e71208a4850f3edd
Jakub Stawski <jakub.stawski@interactive.net.pl>
2023-01-31 13:45:05 +0100

    Oczyszczenie projektu

--- api generator.old/openapi.bat
+++
@@ -1,3 +0,0 @@
-call npx @apidevtools/swagger-cli bundle openapi.yaml --outfile ./build/merged.yaml --type yaml  --enable-post-process-file true
-call npx @openapitools/openapi-generator-cli generate -i ./build/merged.yaml -g csharp-netcore-functions -o ../backend
-@REM call npx @openapitools/openapi-generator-cli generate -i ./build/merged.yaml -g typescript-fetch -o ../frontend/src
